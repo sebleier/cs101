7b3e28b1a30473218a57d8b007a415f3d39f366a
Sean Bleier <sebleier@gmail.com>
2009-09-26 14:10:24 -0500

    Updated driver program
    I included a random sample, nearly ascending ordered sample, and nearly
    desending ordered sample.

--- sorts/main.c
+++ sorts/main.c
@@ -1,31 +1,84 @@
+/*
+    Sorting Algorithm Driver:
+    =========================
+
+    The driver program will create three arrays of size n.  One array will be
+    a set of completely pseudo-random numbers, seeded by time.  The other two
+    will be nearly sorted, one in ascending and one in decending.  To break up
+    the ordered arrays, I'm going to start on both ends and every
+    (50 + rand() % 50)th element going inward will be swapped until the right
+    side is less than the left side.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include "utils.h"
 #include "quicksort.h"
 
+#define MIN_SPAN 50 // minimum span between elements swapped
+#define VAR 50 // variability span
 
-// Driver program to create a sample of a certain amount of pseudo-random
-// integers seeded by the time.  It also tracks the amount of time needed
-// to execute.
+void skew(int *l, int n) {
+    int i, j;
+    i = 0;
+    j = n - 1;
+    while(i < j) {
+        swap(l, i, j);
+        i += MIN_SPAN + rand() % VAR;
+        j -= MIN_SPAN + rand() % VAR;
+    }
+}
+
 int main(int argc, char *argv[]) {
-    int i;
+    int i, j;
     clock_t tick_count, cycles;
     int n = 20000000;
-    int *sample = malloc(n * sizeof (int));
 
     // initialize random sample
+    printf("Initialize random sample\n");
+    int *random = malloc(n * sizeof (int));
     srand(time(NULL));
     for(i = 0; i < n; i++) {
-        sample[i] = rand() % n;
+        random[i] = rand() % n;
     }
-    //print(sample, n);
+
+    // initialize ascending sample
+    printf("Initialize ascending sample\n");
+    int *ascending = malloc(n * sizeof (int));
+    for(i = 0; i < n; i++) {
+        ascending[i] = i;
+    }
+    skew(ascending, n);
+
+    // initialize decending sample
+    printf("Initialize descending sample\n");
+    int *descending = malloc(n * sizeof (int));
+    for(i = 0; i < n; i++) {
+        descending[n - 1 - i] = i;
+    }
+    skew(descending, n);
+
 	tick_count = clock();
     // timer started
-    quicksort(sample, 0, n - 1);
+    quicksort(random, 0, n - 1);
     // stop timer
-    //print(sample, n);
     cycles = clock() - tick_count;
-	printf("Time: %f\n", (float)cycles / (float)CLOCKS_PER_SEC);
+	printf("Random Time: %f\n", (float)cycles / (float)CLOCKS_PER_SEC);
+
+	tick_count = clock();
+    // timer started
+    quicksort(ascending, 0, n - 1);
+    // stop timer
+    cycles = clock() - tick_count;
+	printf("Near Ascending Order Time: %f\n", (float)cycles / (float)CLOCKS_PER_SEC);
+
+	tick_count = clock();
+    // timer started
+    quicksort(descending, 0, n - 1);
+    // stop timer
+    cycles = clock() - tick_count;
+	printf("Near Descending Order Time: %f\n", (float)cycles / (float)CLOCKS_PER_SEC);
 
     return 0;
 }
--- sorts/utils.h
+++ sorts/utils.h
@@ -1,3 +1,6 @@
+#ifndef SORTS_UTILS_H
+#define SORTS_UTILS_H
+#include <stdio.h>
 // Swap elements in position i and j in list l
 void swap(int *l, int i, int j) {
     int temp = l[i];
@@ -5,12 +8,11 @@ void swap(int *l, int i, int j) {
     l[j] = temp;
 }
 
-// Print all elements in the list l
 void print(int *l, int n) {
     int i;
     for(i = 0; i < n; i++) {
         printf("%d, ", l[i]);
     }
-    printf("\n");
-    return;
+    printf("\n\n");
 }
+#endif
